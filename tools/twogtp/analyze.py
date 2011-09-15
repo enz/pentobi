a065440da81d2235ab5608e27be615fac339dde1
Markus Enzenberger <enz@users.sourceforge.net>
2011-09-15 13:28:28 +0200

    More statistical info

--- tools/twogtp/analyze.py
+++ tools/twogtp/analyze.py
@@ -4,16 +4,59 @@ from getopt import getopt
 from math import sqrt
 from sys import argv, exit, stdin
 
-def err(f):
-    return sqrt(f * (1 - f) / games)
+class Statistics:
+    def __init__(self, name):
+        self._name = name
+        self._sum = 0.0
+        self._sum_sq = 0.0
+        self._count = 0
 
-def print_percent(name, n, games):
-    f = float(n) / games
-    print "%s: %.1f%%+-%.1f%%" % (name, 100 * f, 100 * err(f))
+    def add(self, value):
+        self._sum += value
+        self._sum_sq += (value * value)
+        self._count += 1
 
-def print_count(name, n):
-    f = float(n) / games
-    print "%s:\t%i (%.1f%%+-%.1f%%)" % (name, n, 100 * f, 100 * err(f))
+    def get_name(self):
+        return self._name
+
+    def get_count(self):
+        return self._count
+
+    def get_deviation(self):
+        return sqrt(self.get_variance())
+
+    def get_error_mean(self):
+        return self.get_deviation() / sqrt(self._count)
+
+    def get_mean(self):
+        return self._sum / self._count
+
+    def get_variance(self):
+        mean = self.get_mean()
+        return self._sum_sq / self._count - mean * mean
+
+
+def print_stat(statistics):
+    if statistics.get_count() > 0:
+        print "%s:\t%.1f+-%.1f" \
+            % (statistics.get_name(),
+               statistics.get_mean(),
+               statistics.get_error_mean())
+
+def print_stat_percent(statistics):
+    if statistics.get_count() > 0:
+        print "%s:\t%.1f%%+-%.1f%%" \
+            % (statistics.get_name(),
+               100 * statistics.get_mean(),
+               100 * statistics.get_error_mean())
+
+def print_stat_percent_count(statistics):
+    if statistics.get_count() > 0:
+        print "%s:\t%i (%.1f%%+-%.1f%%)" \
+            % (statistics.get_name(),
+               statistics.get_count(),
+               100 * statistics.get_mean(),
+               100 * statistics.get_error_mean())
 
 print_result = False
 opts, args = getopt(argv[1:], "r", [
@@ -24,17 +67,16 @@ for opt, val in opts:
         print_result = True
 
 games = 0
-win = 0
-loss = 0
-draw = 0
-result = 0.0
-games_color_black = 0
-games_color_white = 0
-result_color_black = 0.0
-result_color_white = 0.0
+win = Statistics("Win")
+loss = Statistics("Loss")
+draw = Statistics("Draw")
+result = Statistics("Result")
+score_stat = Statistics("Score")
+result_color_black = Statistics("ResB")
+result_color_white = Statistics("ResW")
 alternate_used = False
-cpu_black = 0.0
-cpu_white = 0.0
+cpu_black = Statistics("CpuB")
+cpu_white = Statistics("CpuW")
 for line in stdin.readlines():
     if line.strip().startswith("#"):
         continue
@@ -45,8 +87,8 @@ for line in stdin.readlines():
     score_white = columns[2]
     length = int(columns[3])
     exchange_color = (columns[4].strip() == "True")
-    cpu_black += float(columns[5])
-    cpu_white += float(columns[6])
+    cpu_black.add(float(columns[5]))
+    cpu_white.add(float(columns[6]))
     if exchange_color:
         alternate_used = True
     if score_black.startswith("B+"):
@@ -57,41 +99,46 @@ for line in stdin.readlines():
         score = 0
     else:
         exit("Invalid score: " + score_black)
+    score_stat.add(score)
     if score > 0:
-        win += 1
+        win.add(1)
+        loss.add(0)
+        draw.add(0)
         result_value = 1.0
     elif score < 0:
-        loss += 1
+        win.add(0)
+        loss.add(1)
+        draw.add(0)
         result_value = 0.0
     elif score == 0:
-        draw += 1
+        win.add(0)
+        loss.add(0)
+        draw.add(1)
         result_value = 0.5
-    result += result_value
+    result.add(result_value)
     if exchange_color:
-        result_color_white += result_value
-        games_color_white += 1
+        result_color_white.add(result_value)
     else:
-        result_color_black += result_value
-        games_color_black += 1
+        result_color_black.add(result_value)
 
 if print_result:
-    f = float(result) / games
     print "%.2f\t%.2f\t%.2f\t%.2f\t%i" \
-        % (100 * f, 100 * err(f), (cpu_black / games), (cpu_white / games),
+        % (100 * result.get_mean(),
+           100 * result.get_error_mean(),
+           cpu_black.get_mean(),
+           cpu_whiteget_mean(),
            games)
 else:
     print "Games:\t%i" % games
+    print_stat_percent(result)
     if games > 0:
-        # TODO: compute real standard error for result, using the formula
-        # for binary results overestimates the error, because there are also
-        # draws (=0.5)
-        print_percent("Result", result, games)
         if alternate_used:
-            print_percent("ResultB", result_color_black, games_color_black)
-            print_percent("ResultW", result_color_white, games_color_white)
-        print_count("Win", win)
-        print_count("Loss", loss)
-        print_count("Draw", draw)
-        print "CpuB:\t%.1f" % (cpu_black / games)
-        print "CpuW:\t%.1f" % (cpu_white / games)
+            print_stat_percent(result_color_black)
+            print_stat_percent(result_color_white)
+        print_stat_percent_count(win)
+        print_stat_percent_count(loss)
+        print_stat_percent_count(draw)
+        print_stat(cpu_black)
+        print_stat(cpu_white)
+        print_stat(score_stat)
 
