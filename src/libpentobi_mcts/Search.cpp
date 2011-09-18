881e3eb6c015e0035f6f20489d78ece31d39d326
Markus Enzenberger <enz@users.sourceforge.net>
2011-09-18 14:10:30 +0200

    Set widening parameter to 8

--- src/libpentobi_mcts/Search.cpp
+++ src/libpentobi_mcts/Search.cpp
@@ -24,6 +24,7 @@ Search::Search(const Board& bd)
     set_unexplored_value(0.5);
     set_expand_threshold(1);
     set_bias_term_constant(0.1f);
+    set_widening_parameter(8);
 }
 
 Search::~Search() throw()
