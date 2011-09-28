9f0d2ea012a40b9eee03051a4520bd8b6ad8674f
Markus Enzenberger <enz@users.sourceforge.net>
2011-09-28 10:40:04 +0200

    Add script to split multi-game SGF files produced by twogtp.py

---
+++ tools/twogtp/split-gamefile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# Split multi-game SGF file produced by twogtp.py into single-game files.
+
+from re import search
+from sys import stdin
+
+def write_file():
+    with open(filename, "a") as f:
+        f.write(buffer)
+
+filename = ""
+buffer = ""
+for line in stdin.readlines():
+    match = search("GN\[(\d+)\]", line)
+    if match:
+        if filename != "":
+            write_file()
+        filename = match.group(1) + ".blksgf"
+        buffer = ""
+    buffer += line
+write_file()
