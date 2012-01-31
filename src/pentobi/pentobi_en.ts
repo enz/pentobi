fd67c56c8e53820785d7d0655bdcac67c280dc36
Markus Enzenberger <enz@users.sourceforge.net>
2012-01-31 20:33:37 +0100

    Add English translations for text containing plurals.

---
+++ src/pentobi/pentobi_en.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message numerus="yes">
+        <location filename="MainWindow.cpp" line="2418"/>
+        <source>%n move(s)</source>
+        <translation>
+            <numerusform>%n move</numerusform>
+            <numerusform>%n moves</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="MainWindow.cpp" line="2446"/>
+        <source>%n move(s) in variation %1</source>
+        <translation>
+            <numerusform>%n move in variation %1</numerusform>
+            <numerusform>%n moves in variation %1</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="MainWindow.cpp" line="2495"/>
+        <source>Blue wins with %n point(s).</source>
+        <translation>
+            <numerusform>Blue wins with %n point.</numerusform>
+            <numerusform>Blue wins with %n points.</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="MainWindow.cpp" line="2497"/>
+        <source>Green wins with %n point(s).</source>
+        <translation>
+            <numerusform>Green wins with %n point.</numerusform>
+            <numerusform>Green wins with %n points.</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="MainWindow.cpp" line="2507"/>
+        <source>Blue/Red wins with %n point(s).</source>
+        <translation>
+            <numerusform>Blue/Red wins with %n point.</numerusform>
+            <numerusform>Blue/Red wins with %n points.</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="MainWindow.cpp" line="2509"/>
+        <source>Yellow/Green wins with %n point(s).</source>
+        <translation>
+            <numerusform>Yellow/Green wins with %n point.</numerusform>
+            <numerusform>Yellow/Green wins with %n points.</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
