0f91dc67e7fe124ef1486b3be13426c2dbb5a468
Markus Enzenberger <enz@users.sourceforge.net>
2012-03-02 10:08:26 +0100

    Fixed computation of the font bounding box in the score display

--- src/libpentobi_gui/ScoreDisplay.cpp
+++ src/libpentobi_gui/ScoreDisplay.cpp
@@ -28,6 +28,7 @@ ScoreDisplay::ScoreDisplay(QWidget* parent)
     m_gameVariant = game_variant_classic;
     m_points.fill(0);
     m_bonus.fill(0);
+    m_font.setStyleStrategy(QFont::PreferOutline);
     setMinimumWidth(300);
     setMinimumHeight(15);
     setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
@@ -104,7 +105,7 @@ int ScoreDisplay::getTextWidth(QString text) const
 {
     // Make text width only depend on number of digits to avoid frequent small
     // changes to the layout
-    QFontMetrics metrics(font());
+    QFontMetrics metrics(m_font);
     int maxDigitWidth = 0;
     maxDigitWidth = max(maxDigitWidth, metrics.width('0'));
     maxDigitWidth = max(maxDigitWidth, metrics.width('1'));
@@ -124,13 +125,9 @@ void ScoreDisplay::paintEvent(QPaintEvent* event)
 {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, true);
-    QFont font;
-    font.setStyleStrategy(QFont::PreferOutline);
-    qreal fontSize = 0.7 * height();
-    font.setPointSizeF(fontSize);
-    painter.setFont(font);
-    m_colorDotSize = 0.8 * fontSize;
-    m_colorDotSpace = 0.3 * fontSize;
+    painter.setFont(m_font);
+    m_colorDotSize = 0.8 * m_fontSize;
+    m_colorDotSpace = 0.3 * m_fontSize;
     m_colorDotWidth = m_colorDotSize + m_colorDotSpace;
     m_twoColorDotWidth = 2 * m_colorDotSize + m_colorDotSpace;
     if (m_gameVariant == game_variant_duo)
@@ -208,6 +205,12 @@ void ScoreDisplay::paintEvent(QPaintEvent* event)
         x+= m_colorDotWidth + textWidthRed + pad;
         drawScore(painter, Color(3), x);
     }
+}
+
+void ScoreDisplay::resizeEvent(QResizeEvent* event)
+{
+    m_fontSize = 0.7 * height();
+    m_font.setPointSizeF(m_fontSize);
 }
 
 void ScoreDisplay::updateScore(const Board& bd)
