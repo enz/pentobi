enz/pentobi

commit 6284f9ecee7b7aff2cceb26e1df3991ecaf29fc9

Fix bug: libboardgame_sgf::Node::remove_property() did not work if first property should be removed.

src/libboardgame_sgf/Node.cpp
@@ -194,6 +194,8 @@ bool Node::remove_property(const string& id)
         return false;
     if (last != 0)
         last->m_next = move(property->m_next);
+    else
+        m_first_property = move(property->next);
     return true;
 }
 

commit cbea00cd31c7eedb11bcb06c6111e7e4d02b2b4b

Fix broken compilation caused by commit 6284f9ec

src/libboardgame_sgf/Node.cpp
@@ -195,7 +195,7 @@ bool Node::remove_property(const string& id)
     if (last != 0)
         last->m_next = move(property->m_next);
     else
-        m_first_property = move(property->next);
+        m_first_property = move(property->m_next);
     return true;
 }
 

src/libboardgame_sgf/Node.h
@@ -27,7 +27,7 @@ struct Property
 
     vector<string> m_values;
 
-    shared_ptr<Property> m_next;
+    unique_ptr<Property> m_next;
 
     Property(const string& id, const vector<string>& values);
 };