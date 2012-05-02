f1e2afe1c4447330b4c59b8f9c9025815a87e431
Markus Enzenberger <enz@users.sourceforge.net>
2012-05-02 11:09:22 +0200

    Fix for last change if computer plays more than one color

--- src/pentobi/MainWindow.cpp
+++ src/pentobi/MainWindow.cpp
@@ -1584,9 +1584,12 @@ void MainWindow::genMove()
                           m_genMoveId);
     m_genMoveWatcher.setFuture(future);
     m_isGenMoveRunning = true;
-    if (m_lastComputerMovesBegin == 0)
+    const Board& bd = getBoard();
+    unsigned int nuMoves = bd.get_nu_moves();
+    if (m_lastComputerMovesBegin == 0
+        || (nuMoves > 0 && bd.get_move(nuMoves - 1).color != m_currentColor))
     {
-        m_lastComputerMovesBegin = getBoard().get_nu_moves() + 1;
+        m_lastComputerMovesBegin = nuMoves + 1;
         m_lastComputerMovesEnd = m_lastComputerMovesBegin;
     }
 }
