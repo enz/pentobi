enz/pentobi

commit 16da9bf6d4d88c70743ee89d5310bf3627695d9c

Move specific definition of game result that is only used in MCTS from class Board to MCTS state.

src/libpentobi_base/Engine.cpp
@@ -94,11 +94,10 @@ void Engine::cmd_final_score(Response& response)
     }
     else
     {
-        double game_result;
-        int score = bd.get_score(Color(0), game_result);
-        if (game_result == 1)
+        int score = bd.get_score(Color(0));
+        if (score > 0)
             response << format("B+%1%") % score;
-        else if (game_result == 0)
+        else if (score == 0)
             response << format("W+%1%") % -score;
         else
             response << "0";

src/libpentobi_base/Board.cpp
@@ -272,56 +272,28 @@ unsigned int Board::get_points_left(Color c) const
     return n;
 }
 
-int Board::get_score(Color c, double& game_result) const
+int Board::get_score(Color c) const
 {
     if (m_variant == variant_duo || m_variant == variant_junior)
     {
         unsigned int points0 = get_points_with_bonus(Color(0));
         unsigned int points1 = get_points_with_bonus(Color(1));
         if (c == Color(0))
-        {
-            if (points0 > points1)
-                game_result = 1;
-            else if (points0 < points1)
-                game_result = 0;
-            else
-                game_result = 0.5;
             return points0 - points1;
-        }
         else
-        {
-            if (points1 > points0)
-                game_result = 1;
-            else if (points1 < points0)
-                game_result = 0;
-            else
-                game_result = 0.5;
             return points1 - points0;
-        }
     }
     else if (m_variant == variant_classic
              || m_variant == variant_trigon
              || m_variant == variant_trigon_3)
     {
-        array<unsigned int,Color::range> points_array;
-        for (unsigned int i = 0; i < m_nu_colors; ++i)
-            points_array[i] = get_points_with_bonus(Color(i));
-        unsigned int points = points_array[c.to_int()];
         int score = 0;
-        for (unsigned int i = 0; i < m_nu_colors; ++i)
-            if (i != c.to_int())
-                score -= points_array[i];
-        score = score / (static_cast<int>(m_nu_colors) - 1) + points;
-        sort(points_array.begin(), points_array.begin() + m_nu_colors);
-        game_result = 0;
-        unsigned int n = 0;
-        for (unsigned int i = 0; i < m_nu_colors; ++i)
-            if (points_array[i] == points)
-            {
-                game_result += static_cast<double>(i) / (m_nu_colors - 1);
-                ++n;
-            }
-        game_result /= n;
+        for (ColorIterator i(m_nu_colors); i; ++i)
+            if (*i != c)
+                score -= get_points_with_bonus(*i);
+        score =
+            get_points_with_bonus(c)
+            + score / (static_cast<int>(m_nu_colors) - 1);
         return score;
     }
     else
@@ -333,25 +305,9 @@ int Board::get_score(Color c, double& game_result) const
         unsigned int points1 =
             get_points_with_bonus(Color(1)) + get_points_with_bonus(Color(3));
         if (c == Color(0) || c == Color(2))
-        {
-            if (points0 > points1)
-                game_result = 1;
-            else if (points0 < points1)
-                game_result = 0;
-            else
-                game_result = 0.5;
             return points0 - points1;
-        }
         else
-        {
-            if (points1 > points0)
-                game_result = 1;
-            else if (points1 < points0)
-                game_result = 0;
-            else
-                game_result = 0.5;
             return points1 - points0;
-        }
     }
 }
 

src/libpentobi_base/Board.h
@@ -290,21 +290,8 @@ public:
     /** Get score.
         The score is the number of points for a color minus the number of
         points of the opponent (or the average score of the opponents if there
-        are more than two players).
-        @param c
-        @param[out] game_result The game result from the point of view of the
-        color: 1=win, 0=loss, 0.5=draw. If there are n &gt; 2 players, this is
-        generalized in the following way: The scores are sorted in ascending
-        order. Each rank r_i (i in 0..n-1) is assigned a result value of
-        r_i/(n-1). If a multiple players have the same score, the result value
-        is the average of all ranks with this score. So being the single winner
-        still gives the result 1 and having the lowest score gives the result
-        0. Being the single winner is better than sharing the best place,
-        which is better than getting the second place, etc.
-        @todo The score should be a floating point type because averaging the
-        points of the opponents in the four-player version leads to fractional
-        points. */
-    int get_score(Color c, double& game_result) const;
+        are more than two players). */
+    int get_score(Color c) const;
 
     /** Get the place of a player in the game result.
         @param c The color of the player.

src/libpentobi_mcts/State.cpp
@@ -36,6 +36,7 @@ using libpentobi_base::variant_duo;
 using libpentobi_base::variant_junior;
 using libpentobi_base::variant_trigon;
 using libpentobi_base::variant_trigon_2;
+using libpentobi_base::variant_trigon_3;
 using libpentobi_base::BoardIterator;
 using libpentobi_base::BoardType;
 using libpentobi_base::ColorIterator;
@@ -101,6 +102,93 @@ Point find_best_starting_point(const Board& bd, Color c)
         }
     }
     return best;
+}
+
+/** Get game result from the view point of a color.
+    The result is 0,0.5,1 for loss/tie/win in 2-player variants. If there are
+    n &gt; 2 players, this is generalized in the following way: The scores are
+    sorted in ascending order. Each rank r_i (i in 0..n-1) is assigned a result
+    value of r_i/(n-1). If a multiple players have the same score, the result
+    value is the average of all ranks with this score. So being the single
+    winner still gives the result 1 and having the lowest score gives the
+    result 0. Being the single winner is better than sharing the best place,
+    which is better than getting the second place, etc. */
+ValueType get_result(const Board& bd, Color c)
+{
+    Variant variant = bd.get_variant();
+    if (variant == variant_duo || variant == variant_junior)
+    {
+        unsigned int points0 = bd.get_points_with_bonus(Color(0));
+        unsigned int points1 = bd.get_points_with_bonus(Color(1));
+        if (c == Color(0))
+        {
+            if (points0 > points1)
+                return 1;
+            else if (points0 < points1)
+                return 0;
+            else
+                return 0.5;
+        }
+        else
+        {
+            if (points1 > points0)
+                return 1;
+            else if (points1 < points0)
+                return 0;
+            else
+                return 0.5;
+        }
+    }
+    else if (variant == variant_classic
+             || variant == variant_trigon
+             || variant == variant_trigon_3)
+    {
+        unsigned int nu_colors = bd.get_nu_colors();
+        array<unsigned int,Color::range> points_array;
+        for (unsigned int i = 0; i < nu_colors; ++i)
+            points_array[i] = bd.get_points_with_bonus(Color(i));
+        unsigned int points = points_array[c.to_int()];
+        sort(points_array.begin(), points_array.begin() + nu_colors);
+        ValueType result = 0;
+        unsigned int n = 0;
+        for (unsigned int i = 0; i < nu_colors; ++i)
+            if (points_array[i] == points)
+            {
+                result += ValueType(i) / (nu_colors - 1);
+                ++n;
+            }
+        result /= n;
+        return result;
+    }
+    else
+    {
+        LIBBOARDGAME_ASSERT(variant == variant_classic_2
+                            || variant == variant_trigon_2);
+        unsigned int points0 =
+            bd.get_points_with_bonus(Color(0))
+            + bd.get_points_with_bonus(Color(2));
+        unsigned int points1 =
+            bd.get_points_with_bonus(Color(1))
+            + bd.get_points_with_bonus(Color(3));
+        if (c == Color(0) || c == Color(2))
+        {
+            if (points0 > points1)
+                return 1;
+            else if (points0 < points1)
+                return 0;
+            else
+                return 0.5;
+        }
+        else
+        {
+            if (points1 > points0)
+                return 1;
+            else if (points1 < points0)
+                return 0;
+            else
+                return 0.5;
+        }
+    }
 }
 
 /** Return the symmetric point state for symmetry detection.
@@ -361,8 +449,8 @@ array<ValueType, 4> State::evaluate_terminal()
     array<ValueType, 4> result_array;
     for (ColorIterator i(m_bd.get_nu_colors()); i; ++i)
     {
-        double game_result;
-        ValueType score = ValueType(m_bd.get_score(*i, game_result));
+        ValueType score = m_bd.get_score(*i);
+        ValueType game_result = get_result(m_bd, *i);
         ValueType score_modification = m_shared_const.score_modification;
         // Apply score modification. Example: If score modification is 0.1,
         // the game result is rescaled to [0..0.9] and the score modification
@@ -401,8 +489,7 @@ bool State::gen_and_play_playout_move(Move last_good_reply)
                      || variant == variant_trigon_2)
                     && ! m_has_moves[m_bd.get_second_color(to_play)])))
         {
-            double game_result;
-            if (m_bd.get_score(to_play, game_result) < 0)
+            if (m_bd.get_score(to_play) < 0)
             {
                 if (log_simulations)
                     log() << "Terminate early (no moves and negative score)\n";

src/pentobi/MainWindow.cpp
@@ -1734,8 +1734,7 @@ void MainWindow::gameOver()
     QString info;
     if (variant == variant_duo || variant == variant_junior)
     {
-        double game_result;
-        int score = bd.get_score(Color(0), game_result);
+        int score = bd.get_score(Color(0));
         if (score > 0)
             info = tr("Blue wins with %n point(s).", "", score);
         else if (score < 0)
@@ -1745,8 +1744,7 @@ void MainWindow::gameOver()
     }
     else if (variant == variant_classic_2 || variant == variant_trigon_2)
     {
-        double game_result;
-        int score = bd.get_score(Color(0), game_result);
+        int score = bd.get_score(Color(0));
         if (score > 0)
             info = tr("Blue/Red wins with %n point(s).", "", score);
         else if (score < 0)

commit 2bba00ae70f7a7383ac0446c40173051ea6f0989

Fix bug in final_score GTP command introduced by recent commit (16da9bf6)

src/libpentobi_base/Engine.cpp
@@ -84,10 +84,7 @@ void Engine::cmd_clear_board()
 void Engine::cmd_final_score(Response& response)
 {
     const Board& bd = get_board();
-    Variant variant = bd.get_variant();
-    if (variant == variant_classic
-        || variant == variant_trigon
-        || variant == variant_trigon_3)
+    if (get_nu_players(bd.get_variant()) > 2)
     {
         for (ColorIterator i(bd.get_nu_colors()); i; ++i)
             response << bd.get_points_with_bonus(*i) << ' ';
@@ -97,7 +94,7 @@ void Engine::cmd_final_score(Response& response)
         int score = bd.get_score(Color(0));
         if (score > 0)
             response << format("B+%1%") % score;
-        else if (score == 0)
+        else if (score < 0)
             response << format("W+%1%") % -score;
         else
             response << "0";