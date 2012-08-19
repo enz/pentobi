75d97d1d374232b19af1ea63f99056ae58998bc9
Markus Enzenberger <enz@users.sourceforge.net>
2012-08-19 15:09:59 +0200

    Added code to build a KDE thumbnailer

---
+++ src/pentobi_kde_thumbnailer/PentobiThumbCreator.cpp
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------------
+/** @file pentobi_kde_thumbnailer/PentobiThumbCreator.cpp */
+//-----------------------------------------------------------------------------
+
+#include "PentobiThumbCreator.h"
+
+#include "libpentobi_thumbnail/CreateThumbnail.h"
+
+using namespace std;
+
+//-----------------------------------------------------------------------------
+
+extern "C"
+{
+    KDE_EXPORT ThumbCreator* new_creator()
+    {
+        return new PentobiThumbCreator;
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+PentobiThumbCreator::~PentobiThumbCreator()
+{
+}
+
+bool PentobiThumbCreator::create(const QString& path, int width, int height,
+                                 QImage& image)
+{
+    image = QImage(width, height, QImage::Format_ARGB32);
+    image.fill(Qt::transparent);
+    return createThumbnail(path, width, height, image);
+}
+
+//-----------------------------------------------------------------------------
---
+++ src/pentobi_kde_thumbnailer/PentobiThumbCreator.h
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------
+/** @file pentobi_kde_thumbnailer/PentobiThumbCreator.h */
+//-----------------------------------------------------------------------------
+
+#ifndef PENTOBI_KDE_THUMBNAILER_PENTOBI_THUMB_CREATOR_H
+#define PENTOBI_KDE_THUMBNAILER_PENTOBI_THUMB_CREATOR_H
+
+#include <QObject>
+#include <kio/thumbcreator.h>
+
+//-----------------------------------------------------------------------------
+
+class PentobiThumbCreator
+    : public QObject,
+      public ThumbCreator
+{
+    Q_OBJECT
+
+public:
+    virtual ~PentobiThumbCreator();
+
+    bool create(const QString& path, int width, int height, QImage& image);
+};
+
+//-----------------------------------------------------------------------------
+
+#endif // PENTOBI_KDE_THUMBNAILER_PENTOBI_THUMB_CREATOR_H
