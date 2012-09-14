0be8a382f16e676542710ab5a37c464899e63b4b
Markus Enzenberger <enz@users.sourceforge.net>
2012-09-14 08:52:48 +0200

    Added comment

--- src/libboardgame_util/TimeSource.h
+++ src/libboardgame_util/TimeSource.h
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-/** @file TimeSource.h */
+/** @file libboardgame_util/TimeSource.h */
 //-----------------------------------------------------------------------------
 
 #ifndef LIBBOARDGAME_UTIL_TIME_SOURCE_H
@@ -9,6 +9,11 @@ namespace libboardgame_util {
 
 //-----------------------------------------------------------------------------
 
+/** Abstract time source for measuring thinking times for move generation.
+    Typical implementations are wall time, CPU time or mock time sources
+    for unit tests. They do not need to provide high resolutions (but should
+    support at least 100 ms) and should support maximum times of days (or even
+    months). */
 class TimeSource
 {
 public:
