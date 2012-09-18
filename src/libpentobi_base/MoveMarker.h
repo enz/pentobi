enz/pentobi

commit d2cb3fbc99ae9cfc9789e526ef95ad33e9c33a58

Added MoveMarker::clear_all_set_known() and MoveMarker::test_and_set() that are faster than the previously used ones.

src/libpentobi_base/MoveMarker.h
@@ -5,7 +5,8 @@
 #ifndef LIBPENTOBI_BASE_MOVE_MARKER_H
 #define LIBPENTOBI_BASE_MOVE_MARKER_H
 
-#include <bitset>
+#include <climits>
+#include <cstring>
 #include "Move.h"
 #include "libboardgame_util/ArrayList.h"
 
@@ -30,6 +31,13 @@ public:
     template<unsigned int M>
     void clear(const ArrayList<Move, M>& moves);
 
+    /** Clear all moves from a list optimized for the case when we know
+        that the list contains all marked moves in the marker.
+        This function assumes that all moves not in the list are not marked
+        and is faster than clear(ArrayList). */
+    template<unsigned int M>
+    void clear_all_set_known(const ArrayList<Move, M>& moves);
+
     void set(Move mv);
 
     /** Mark all moves (slow). */
@@ -37,8 +45,17 @@ public:
 
     bool operator[](Move mv) const;
 
+    /** Mark a move and return whether it was already marked. */
+    bool test_and_set(Move mv);
+
 private:
-    bitset<Move::range> m_marker;
+    static const size_t array_size = Move::range / CHAR_BIT;
+
+    unsigned char m_array[array_size];
+
+    size_t get_index(Move mv) const;
+
+    unsigned char get_mask(Move mv) const;
 };
 
 inline MoveMarker::MoveMarker()
@@ -47,12 +64,12 @@ inline MoveMarker::MoveMarker()
 
 inline bool MoveMarker::operator[](Move mv) const
 {
-    return m_marker[mv.to_int()];
+    return m_array[get_index(mv)] & get_mask(mv);
 }
 
 inline void MoveMarker::clear(Move mv)
 {
-    m_marker[mv.to_int()] = false;
+    m_array[get_index(mv)] &= ~get_mask(mv);
 }
 
 inline void MoveMarker::clear(const vector<Move>& moves)
@@ -72,14 +89,42 @@ inline void MoveMarker::clear(const ArrayList<Move, M>& moves)
         clear(*i);
 }
 
+template<unsigned int M>
+inline void MoveMarker::clear_all_set_known(const ArrayList<Move, M>& moves)
+{
+    auto begin = moves.begin();
+    auto end = moves.end();
+    for (auto i = begin; i != end; ++i)
+        m_array[get_index(*i)] = 0;
+}
+
+inline size_t MoveMarker::get_index(Move mv) const
+{
+    return mv.to_int() / CHAR_BIT;
+}
+
+inline unsigned char MoveMarker::get_mask(Move mv) const
+{
+    return static_cast<unsigned char>(1) << (mv.to_int() % CHAR_BIT);
+}
+
 inline void MoveMarker::set(Move mv)
 {
-    m_marker[mv.to_int()] = true;
+    m_array[get_index(mv)] |= get_mask(mv);
 }
 
 inline void MoveMarker::set_all()
 {
-    m_marker.set();
+    memset(m_array, ~static_cast<unsigned char>(0), array_size);
+}
+
+inline bool MoveMarker::test_and_set(Move mv)
+{
+    unsigned char& elem = m_array[get_index(mv)];
+    unsigned char mask = get_mask(mv);
+    bool result = elem & mask;
+    elem |= mask;
+    return result;
 }
 
 //-----------------------------------------------------------------------------

src/libpentobi_mcts/State.cpp
@@ -262,10 +262,10 @@ inline void State::add_moves(Point p, Color c, Piece piece,
         {
             unsigned int local_value;
             const MoveInfo& info = get_move_info(*i);
-            if (check_move(c, info.points, local_value) && ! m_marker[*i])
+            if (check_move(c, info.points, local_value)
+                && ! m_marker.test_and_set(*i))
             {
                 moves.push_back(*i);
-                m_marker.set(*i);
                 check_local(local_value, *i, info);
             }
         }
@@ -723,7 +723,7 @@ void State::init_move_list_with_local(Color c)
             if (! m_bd.is_forbidden(p, c))
                 add_moves(p, c);
     }
-    m_marker.clear(moves);
+    m_marker.clear_all_set_known(moves);
     m_is_move_list_initialized[c] = true;
     if (moves.empty() && ! m_consider_all_pieces)
     {
@@ -788,7 +788,7 @@ void State::init_move_list_without_local(Color c)
                     }
             }
     }
-    m_marker.clear(moves);
+    m_marker.clear_all_set_known(moves);
     m_is_move_list_initialized[c] = true;
     if (moves.empty() && ! m_consider_all_pieces)
     {
@@ -1080,7 +1080,7 @@ void State::update_move_list(Color c)
         m_is_piece_considered[c] = &is_piece_considered;
     }
 
-    m_marker.clear(m_moves[c]);
+    m_marker.clear_all_set_known(m_moves[c]);
     m_last_move[c] = Move::null();
     if (m_moves[c].empty() && ! m_consider_all_pieces)
     {

commit fc6759bda0a2e3cde0b3bd7775602fb5d9f7482b

Fixed bug introduced in d2cb3fbc99ae9cfc9789e526ef95ad33e9c33a58 MoveMarker constructor did not clear memory.

src/libpentobi_base/MoveMarker.h
@@ -24,6 +24,9 @@ class MoveMarker
 public:
     MoveMarker();
 
+    /** Clear all moves (slow). */
+    void clear();
+
     void clear(Move mv);
 
     void clear(const vector<Move>& moves);
@@ -60,11 +63,17 @@ private:
 
 inline MoveMarker::MoveMarker()
 {
+    clear();
 }
 
 inline bool MoveMarker::operator[](Move mv) const
 {
     return m_array[get_index(mv)] & get_mask(mv);
+}
+
+inline void MoveMarker::clear()
+{
+    memset(m_array, 0, array_size);
 }
 
 inline void MoveMarker::clear(Move mv)