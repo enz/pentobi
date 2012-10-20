f5b65834263ddd1adf2b97d1a48ac82e3753f1f7
Markus Enzenberger <enz@users.sourceforge.net>
2012-10-20 19:29:55 +0200

    Added Canadian English translation

---
+++ src/libpentobi_gui/libpentobi_gui_en_CA.ts
@@ -0,0 +1,154 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_CA">
+<context>
+    <name>ComputerColorDialog</name>
+    <message>
+        <location filename="ComputerColorDialog.cpp" line="34"/>
+        <source>Computer Colors</source>
+        <translation>Computer Colours</translation>
+    </message>
+    <message>
+        <location filename="ComputerColorDialog.cpp" line="40"/>
+        <source>Colors played by the computer:</source>
+        <translation>Colours played by the computer:</translation>
+    </message>
+</context>
+<context>
+    <name>GameInfoDialog</name>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="31"/>
+        <source>Game Info</source>
+        <translation>Game Info</translation>
+    </message>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="44"/>
+        <location filename="GameInfoDialog.cpp" line="50"/>
+        <location filename="GameInfoDialog.cpp" line="57"/>
+        <source>Player Blue:</source>
+        <translation>Player Blue:</translation>
+    </message>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="45"/>
+        <location filename="GameInfoDialog.cpp" line="53"/>
+        <source>Player Green:</source>
+        <translation>Player Green:</translation>
+    </message>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="51"/>
+        <location filename="GameInfoDialog.cpp" line="58"/>
+        <source>Player Yellow:</source>
+        <translation>Player Yellow:</translation>
+    </message>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="52"/>
+        <location filename="GameInfoDialog.cpp" line="59"/>
+        <source>Player Red:</source>
+        <translation>Player Red:</translation>
+    </message>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="65"/>
+        <source>Player Blue/Red:</source>
+        <translation>Player Blue/Red:</translation>
+    </message>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="67"/>
+        <source>Player Yellow/Green:</source>
+        <translation>Player Yellow/Green:</translation>
+    </message>
+    <message>
+        <location filename="GameInfoDialog.cpp" line="69"/>
+        <source>Date:</source>
+        <translation>Date:</translation>
+    </message>
+</context>
+<context>
+    <name>HelpWindow</name>
+    <message>
+        <location filename="HelpWindow.cpp" line="37"/>
+        <source>Pentobi User Manual</source>
+        <translation>Pentobi User Manual</translation>
+    </message>
+    <message>
+        <location filename="HelpWindow.cpp" line="43"/>
+        <source>Back</source>
+        <translation>Back</translation>
+    </message>
+    <message>
+        <location filename="HelpWindow.cpp" line="49"/>
+        <source>Forward</source>
+        <translation>Forward</translation>
+    </message>
+    <message>
+        <location filename="HelpWindow.cpp" line="55"/>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+</context>
+<context>
+    <name>InitialRatingDialog</name>
+    <message>
+        <location filename="InitialRatingDialog.cpp" line="21"/>
+        <source>Initial Rating</source>
+        <translation>Initial Rating</translation>
+    </message>
+    <message>
+        <location filename="InitialRatingDialog.cpp" line="28"/>
+        <source>You have not yet played rated games in this game variant. Estimate your playing strength to initialize your rating.</source>
+        <translation>You have not yet played rated games in this game variant. Estimate your playing strength to initialize your rating.</translation>
+    </message>
+    <message>
+        <location filename="InitialRatingDialog.cpp" line="35"/>
+        <source>Beginner</source>
+        <translation>Beginner</translation>
+    </message>
+    <message>
+        <location filename="InitialRatingDialog.cpp" line="42"/>
+        <source>Expert</source>
+        <translation>Expert</translation>
+    </message>
+    <message>
+        <location filename="InitialRatingDialog.cpp" line="57"/>
+        <source>Your initial rating: %1</source>
+        <translation>Your initial rating: %1</translation>
+    </message>
+</context>
+<context>
+    <name>Util</name>
+    <message>
+        <location filename="Util.cpp" line="235"/>
+        <location filename="Util.cpp" line="242"/>
+        <location filename="Util.cpp" line="253"/>
+        <source>Blue</source>
+        <translation>Blue</translation>
+    </message>
+    <message>
+        <location filename="Util.cpp" line="237"/>
+        <location filename="Util.cpp" line="248"/>
+        <source>Green</source>
+        <translation>Green</translation>
+    </message>
+    <message>
+        <location filename="Util.cpp" line="244"/>
+        <location filename="Util.cpp" line="255"/>
+        <source>Yellow</source>
+        <translation>Yellow</translation>
+    </message>
+    <message>
+        <location filename="Util.cpp" line="246"/>
+        <location filename="Util.cpp" line="257"/>
+        <source>Red</source>
+        <translation>Red</translation>
+    </message>
+    <message>
+        <location filename="Util.cpp" line="262"/>
+        <source>Blue/Red</source>
+        <translation>Blue/Red</translation>
+    </message>
+    <message>
+        <location filename="Util.cpp" line="264"/>
+        <source>Yellow/Green</source>
+        <translation>Yellow/Green</translation>
+    </message>
+</context>
+</TS>
