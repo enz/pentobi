b53848e55300ea1603d20eb540737fc98a19e97d
Markus Enzenberger <enz@users.sourceforge.net>
2013-01-04 09:32:46 +0100

    Replace boost::date_time with std::chrono

--- src/libboardgame_util/WallTime.cpp
+++ src/libboardgame_util/WallTime.cpp
@@ -10,15 +10,13 @@
 
 namespace libboardgame_util {
 
+using namespace std::chrono;
+
 //-----------------------------------------------------------------------------
 
 WallTime::WallTime()
 {
-#if LIBBOARDGAME_USE_STD_CHRONO
-    m_start = std::chrono::system_clock::now();
-#else
-    m_start = boost::posix_time::microsec_clock::universal_time();
-#endif
+    m_start = system_clock::now();
 }
 
 double WallTime::operator()()
@@ -26,13 +24,8 @@ double WallTime::operator()()
     // Logically, there is no need to return the time since m_start, we could
     // also use time_since_epoch(), but during debugging it is nicer to
     // deal with smaller numbers.
-#if LIBBOARDGAME_USE_STD_CHRONO
-    auto diff = std::chrono::system_clock::now() - m_start;
-    return std::chrono::duration_cast<duration<double>>(diff).count();
-#else
-    auto diff = boost::posix_time::microsec_clock::universal_time() - m_start;
-    return double(diff.total_nanoseconds()) * 1e-9;
-#endif
+    auto diff = system_clock::now() - m_start;
+    return duration_cast<duration<double>>(diff).count();
 }
 
 //----------------------------------------------------------------------------
--- src/libboardgame_util/WallTime.h
+++ src/libboardgame_util/WallTime.h
@@ -5,16 +5,7 @@
 #ifndef LIBBOARDGAME_UTIL_WALL_TIME_H
 #define LIBBOARDGAME_UTIL_WALL_TIME_H
 
-/** Use std::chrono.
-    Due to compilation errors in std::chrono with CLang 3.0/libstdc++ (as in
-    Ubuntu 12.04), we still use Boost.Date_Time for measuring wall time. */
-#define LIBBOARDGAME_USE_STD_CHRONO 0
-
-#if LIBBOARDGAME_USE_STD_CHRONO
 #include <chrono>
-#else
-#include <boost/date_time/posix_time/posix_time.hpp>
-#endif
 #include "TimeSource.h"
 
 namespace libboardgame_util {
@@ -32,11 +23,7 @@ public:
     double operator()();
 
 private:
-#if LIBBOARDGAME_USE_STD_CHRONO
     std::chrono::system_clock::time_point m_start;
-#else
-    boost::posix_time::ptime m_start;
-#endif
 };
 //-----------------------------------------------------------------------------
 
