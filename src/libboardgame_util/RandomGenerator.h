05d0304f66c57f7ff10c838ae309e5510bc0811d
Markus Enzenberger <enz@users.sourceforge.net>
2013-03-23 08:21:09 +0100

    Removed workaround no longer necessary for GCC >=4.7

--- src/libboardgame_util/RandomGenerator.h
+++ src/libboardgame_util/RandomGenerator.h
@@ -23,18 +23,13 @@ using namespace std;
 class RandomGenerator
 {
 public:
-#if defined __GNUC__ && ! defined __clang__ \
-  && (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 4))
-    typedef mt19937 Generator;
-#else
     /** This generator is the same as mt11213b in the Boost library.
         According to the documentation of Boost 1.52, this generator is a bit
         faster and uses less memory than mt19937. (The typedef mt11213b does
         not exist in C++11.) */
-    typedef mersenne_twister_engine<uint32_t,32,351,175,19,0xccab8ee7,11,
-                                    0xffffffff,7,0x31b6ab00,15,0xffe50000,17,
-                                    1812433253> Generator;
-#endif
+    typedef mersenne_twister_engine<uint32_t, 32, 351, 175, 19, 0xccab8ee7, 11,
+                                    0xffffffff, 7, 0x31b6ab00, 15, 0xffe50000,
+                                    17, 1812433253> Generator;
 
     typedef Generator::result_type ResultType;
 
