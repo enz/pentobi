e36b85669b932480e159457f691450e144ed8298
Markus Enzenberger <enz@users.sourceforge.net>
2013-03-23 09:47:42 +0100

    Use SSE-based random generator if available (non-standard extension of GCC >=4.8)

--- src/libboardgame_util/RandomGenerator.h
+++ src/libboardgame_util/RandomGenerator.h
@@ -9,6 +9,16 @@
 #include <random>
 #include "Assert.h"
 
+// Use fast SSE-based random generator if GCC >=4.8 (non-standard GCC extension)
+#if defined __GNUC__ \
+    && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8))
+#define LIBBOARDGAME_USE_SIMD_MERSENNE_TWISTER
+#endif
+
+#ifdef LIBBOARDGAME_USE_SIMD_MERSENNE_TWISTER
+#include <ext/random>
+#endif
+
 namespace libboardgame_util {
 
 using namespace std;
@@ -23,13 +33,18 @@ using namespace std;
 class RandomGenerator
 {
 public:
-    /** This generator is the same as mt11213b in the Boost library.
+    /** @typedef Generator
+        This generator is the same as mt11213b in the Boost library.
         According to the documentation of Boost 1.52, this generator is a bit
         faster and uses less memory than mt19937. (The typedef mt11213b does
         not exist in C++11.) */
+#ifdef LIBBOARDGAME_USE_SIMD_MERSENNE_TWISTER
+    typedef __gnu_cxx::sfmt11213 Generator;
+#else
     typedef mersenne_twister_engine<uint32_t, 32, 351, 175, 19, 0xccab8ee7, 11,
                                     0xffffffff, 7, 0x31b6ab00, 15, 0xffe50000,
                                     17, 1812433253> Generator;
+#endif
 
     typedef Generator::result_type ResultType;
 
@@ -92,7 +107,7 @@ inline int RandomGenerator::generate_small_int(int n)
 inline unsigned RandomGenerator::generate_small_uint(unsigned n)
 {
     LIBBOARDGAME_ASSERT(n < (1 << 16));
-    unsigned i = static_cast<unsigned>(((m_generator() & 0xffff) * n) >> 16);
+    auto i = static_cast<unsigned>(((m_generator() & 0xffff) * n) >> 16);
     LIBBOARDGAME_ASSERT(i < n);
     return i;
 }
