00b6a694af63555ef371d61f640dd9e2283adfd6
Markus Enzenberger <enz@users.sourceforge.net>
2013-05-18 11:03:52 +0200

    Workaround for non-functioning time measurement with GCC 4.7.3-1ubuntu1 on Ubuntu 13.04

--- src/libboardgame_util/WallTime.cpp
+++ src/libboardgame_util/WallTime.cpp
@@ -10,6 +10,7 @@
 
 namespace libboardgame_util {
 
+using namespace std;
 using namespace std::chrono;
 
 //-----------------------------------------------------------------------------
@@ -25,7 +26,12 @@ double WallTime::operator()()
     // also use time_since_epoch(), but during debugging it is nicer to
     // deal with smaller numbers.
     auto diff = system_clock::now() - m_start;
-    return duration_cast<duration<double>>(diff).count();
+    // Note: Explicitely specify all template arguments to class duration in
+    // the following line even if duration<double> should be enough because
+    // the second parameter defaults to ratio<1>. But if duration<double> is
+    // used, the counts are much too high with GCC 4.7.3-1ubuntu1 on
+    // Ubuntu 13.04 for unknown reasons.
+    return duration_cast<duration<double, ratio<1>>>(diff).count();
 }
 
 //----------------------------------------------------------------------------
