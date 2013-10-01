04ba1432843647a3a5a37e4c1ca351ef5294484e
Markus Enzenberger <enz@users.sourceforge.net>
2013-10-01 16:40:02 +0200

    No longer automatically set LIBBOARDGAME_DEBUG if DEBUG or _DEBUG is defined.

--- src/libboardgame_util/Assert.h
+++ src/libboardgame_util/Assert.h
@@ -29,30 +29,12 @@ void handle_assertion(const char* expression, const char* file, int line);
 
 //-----------------------------------------------------------------------------
 
-/** @def LIBBOARDGAME_DEBUG
-    Determines if the assertions with LIBBOARDGAME_ASSERT are enabled.
-    LIBBOARDGAME_DEBUG needs to be defined and set to a value that is not zero
-    to enable assertions. If LIBBOARDGAME_DEBUG is not explicitely defined it
-    will default to 1 if either DEBUG or _DEBUG are defined (independent of
-    their value) and 0 otherwise.
-    @warning The definition of LIBBOARDGAME_DEBUG must be consistent in all
-    compilation units because the debug versions of some classes in
-    LibBoardGame have additional members and the program will crash if
-    compilation units are linked that do not agree on the size of objects. */
-#ifndef LIBBOARDGAME_DEBUG
-#if defined(DEBUG) || defined(_DEBUG)
-#define LIBBOARDGAME_DEBUG 1
-#else
-#define LIBBOARDGAME_DEBUG 0
-#endif
-#endif
-
 /** @def LIBBOARDGAME_ASSERT
     Enhanced assert macro.
     This macro is similar to the assert macro in the standard library, but it
-    allows to register assertion handlers that are executed before the program
-    is aborted. Assertions are only enabled if LIBBOARDGAME_DEBUG is defined
-    and not zero. */
+    allows the user to register assertion handlers that are executed before the
+    program is aborted. Assertions are only enabled if the macro
+    LIBBOARDGAME_DEBUG is true. */
 #if LIBBOARDGAME_DEBUG
 #define LIBBOARDGAME_ASSERT(expr)                                       \
     ((expr) ? (static_cast<void>(0))                                    \
