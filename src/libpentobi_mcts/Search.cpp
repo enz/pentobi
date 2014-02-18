d730028f03f4b0f1f8d1df15121cea933c3cf3b5
Markus Enzenberger <enz@users.sourceforge.net>
2014-02-18 10:55:19 +0100

    Reduced expand threshold at higher depths

--- src/libpentobi_mcts/Search.cpp
+++ src/libpentobi_mcts/Search.cpp
@@ -292,8 +292,8 @@ void Search::set_default_param(Variant variant)
     log() << "Setting default parameters for " << to_string(variant) << '\n';
     set_skip_bias_term_min(30000);
     set_bias_term_interval(20);
-    set_expand_threshold(0);
-    set_expand_threshold_inc(1.5f);
+    set_expand_threshold(1);
+    set_expand_threshold_inc(0.5f);
     set_rave_weight(0.7f);
     set_rave_child_max(2000);
     // The following parameters are currently tuned for duo, classic_2 and
