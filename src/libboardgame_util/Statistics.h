d1ea4a35c5cbc2534b4087cba5c6e8329e5aeaf9
Markus Enzenberger <enz@users.sourceforge.net>
2014-07-29 08:58:21 +0200

    Avoid negative argument to sqrt() in Statistics::get_deviation() (occured due to rounding errors?)

--- src/libboardgame_util/Statistics.h
+++ src/libboardgame_util/Statistics.h
@@ -165,7 +165,8 @@ inline FLOAT Statistics<FLOAT>::get_count() const
 template<typename FLOAT>
 inline FLOAT Statistics<FLOAT>::get_deviation() const
 {
-    return sqrt(m_variance);
+    // m_variance can become negative (due to rounding errors?)
+    return (m_variance < 0 ? 0 : sqrt(m_variance));
 }
 
 template<typename FLOAT>
