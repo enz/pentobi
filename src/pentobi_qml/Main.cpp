f7dac19086a33fc26a15faea0401ed7dcf955e62
Markus Enzenberger <enz@users.sourceforge.net>
2014-09-20 13:44:37 +0200

    Android: enable log messages only in debug builds

--- src/pentobi_qml/Main.cpp
+++ src/pentobi_qml/Main.cpp
@@ -10,11 +10,16 @@
 #include "PlayerModel.h"
 #include "libboardgame_util/Log.h"
 
+using libboardgame_util::set_log_null;
+
 //-----------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
     libboardgame_util::LogInitializer log_initializer;
+#if QT_NO_DEBUG
+    set_log_null();
+#endif
     qmlRegisterType<BoardModel>("pentobi", 1, 0, "BoardModel");
     qmlRegisterType<PlayerModel>("pentobi", 1, 0, "PlayerModel");
     qmlRegisterInterface<PieceModel>("PieceModel");
