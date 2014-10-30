8887dd8c5cbdbf8704f7fefaff178ed6eb309a35
Markus Enzenberger <enz@users.sourceforge.net>
2014-10-30 16:20:05 +0100

    Removed unused using declaration

--- src/libpentobi_mcts/State.cpp
+++ src/libpentobi_mcts/State.cpp
@@ -16,7 +16,6 @@
 namespace libpentobi_mcts {
 
 using namespace std;
-using libboardgame_util::get_log;
 using libboardgame_util::fast_exp;
 using libpentobi_base::BoardType;
 using libpentobi_base::ColorIterator;
