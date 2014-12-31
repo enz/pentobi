9753ab25c8014fd5289ef97adf03d03d4473f142
Markus Enzenberger <enz@users.sourceforge.net>
2014-12-31 07:56:38 +0100

    Updated copyright year

--- src/pentobi/MainWindow.cpp
+++ src/pentobi/MainWindow.cpp
@@ -355,7 +355,7 @@ void MainWindow::about()
                        "<p>" +
                        tr("Pentobi is a computer opponent for the board game Blokus.")
                        + "<br>" +
-                       tr("Copyright &copy; 2011&ndash;2014 Markus Enzenberger") +
+                       tr("Copyright &copy; 2011&ndash;%1 Markus Enzenberger").arg(2015) +
                        + "<br>" +
                        "<a href=\"http://pentobi.sourceforge.net\">http://pentobi.sourceforge.net</a>"
                        "</p>");
