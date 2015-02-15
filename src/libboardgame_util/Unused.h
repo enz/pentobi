afed0da27814157ca6f48bb1bda848d263d628eb
Markus Enzenberger <enz@users.sourceforge.net>
2015-02-15 12:09:28 +0100

    Removed unused include

--- src/libboardgame_util/Unused.h
+++ src/libboardgame_util/Unused.h
@@ -7,13 +7,9 @@
 #ifndef LIBBOARDGAME_UTIL_UNUSED_H
 #define LIBBOARDGAME_UTIL_UNUSED_H
 
-#include "Assert.h"
-
 //-----------------------------------------------------------------------------
 
-template<class T> static void LIBBOARDGAME_UNUSED(const T&)
-{
-}
+template<class T> static void LIBBOARDGAME_UNUSED(const T&) { }
 
 #if LIBBOARDGAME_DEBUG
 #define LIBBOARDGAME_UNUSED_IF_NOT_DEBUG(x)
