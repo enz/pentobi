63d965e30513758a8b9ffdf72660f80baf21b0c6
Markus Enzenberger <enz@users.sourceforge.net>
2015-04-29 15:15:34 +0200

    Save pentobi_qml translations without location information

--- src/pentobi_qml/qml/translations/pentobi_qml_de.ts
+++ src/pentobi_qml/qml/translations/pentobi_qml_de.ts
@@ -4,43 +4,34 @@
 <context>
     <name>ComputerColorDialog</name>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="14"/>
         <source>Computer Colors</source>
         <translation>Computer-Farben</translation>
     </message>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="18"/>
         <source>Computer plays:</source>
         <translation>Computer spielt:</translation>
     </message>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="29"/>
         <source>Blue/Red</source>
         <translation>Blau/Rot</translation>
     </message>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="31"/>
         <source>Blue</source>
         <translation>Blau</translation>
     </message>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="47"/>
         <source>Yellow/Green</source>
         <translation>Gelb/Grün</translation>
     </message>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="50"/>
-        <location filename="../ComputerColorDialog.qml" line="72"/>
         <source>Green</source>
         <translation>Grün</translation>
     </message>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="52"/>
         <source>Yellow</source>
         <translation>Gelb</translation>
     </message>
     <message>
-        <location filename="../ComputerColorDialog.qml" line="64"/>
         <source>Red</source>
         <translation>Rot</translation>
     </message>
@@ -48,159 +39,89 @@
 <context>
     <name>Main</name>
     <message>
-        <location filename="../Main.qml" line="36"/>
         <source>Pentobi</source>
         <translation>Pentobi</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="19"/>
-        <location filename="../Main.js" line="263"/>
-        <source>New game?</source>
-        <translation>Neues Spiel?</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="312"/>
-        <location filename="../Main.js" line="341"/>
-        <location filename="../Main.js" line="361"/>
-        <source>Blue wins.</source>
-        <translation>Blau gewinnt.</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="314"/>
-        <location filename="../Main.js" line="367"/>
-        <source>Green wins.</source>
-        <translation>Grün gewinnt.</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="316"/>
-        <location filename="../Main.js" line="327"/>
-        <location filename="../Main.js" line="339"/>
-        <location filename="../Main.js" line="359"/>
-        <source>Game ends in a tie.</source>
-        <translation>Spiel endet unentschieden.</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="323"/>
-        <source>Blue/Red wins.</source>
-        <translation>Blau/Rot gewinnt.</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="325"/>
-        <source>Yellow/Green wins.</source>
-        <translation>Gelb/Grün gewinnt.</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="343"/>
-        <location filename="../Main.js" line="363"/>
-        <source>Yellow wins.</source>
-        <translation>Gelb gewinnt.</translation>
-    </message>
-    <message>
-        <location filename="../Main.js" line="345"/>
-        <location filename="../Main.js" line="365"/>
-        <source>Red wins.</source>
-        <translation>Rot gewinnt.</translation>
     </message>
 </context>
 <context>
     <name>Menu</name>
     <message>
-        <location filename="../Menu.qml" line="14"/>
         <source>Game Variant</source>
         <translation>Spielvariante</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="39"/>
         <source>Duo</source>
         <translation>Duo</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="18"/>
         <source>Classic (4 Players)</source>
         <translation>Klassisch (4 Spieler)</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="7"/>
         <source>Game</source>
         <translation>Spiel</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="79"/>
         <source>Find Move</source>
         <translation>Zug finden</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="88"/>
         <source>Computer</source>
         <translation>Computer</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="95"/>
         <source>Play</source>
         <translation>Spielen</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="32"/>
         <source>Classic (2 Players)</source>
         <translation>Klassisch (2 Spieler)</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="10"/>
         <source>New</source>
         <translation>Neu</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="25"/>
         <source>Classic (3 Players)</source>
         <translation>Klassisch (3 Spieler)</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="46"/>
         <source>Trigon (4 Players)</source>
         <translation>Trigon (4 Spieler)</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="53"/>
         <source>Trigon (3 Players)</source>
         <translation>Trigon (3 Spieler)</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="60"/>
         <source>Trigon (2 Players)</source>
         <translation>Trigon (2 Spieler)</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="67"/>
         <source>Junior</source>
         <translation>Junior</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="75"/>
         <source>Undo Move</source>
         <translation>Zug rückgängig</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="91"/>
         <source>Computer Colors</source>
         <translation>Computer-Farben</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="99"/>
         <source>Level</source>
         <translation>Spielstufe</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="113"/>
         <source>View</source>
         <translation>Ansicht</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="116"/>
         <source>Mark Last Move</source>
         <translation>Letzten Zug markieren</translation>
     </message>
     <message>
-        <location filename="../Menu.qml" line="83"/>
         <source>Quit</source>
         <translation>Beenden</translation>
     </message>
