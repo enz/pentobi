82c5e74328385f6ba6118e875e9ab66e33750219
Markus Enzenberger <enz@users.sourceforge.net>
2015-09-07 09:30:28 +0200

    Make PentobiThumbCreator::create() return false if the QImage could not be allocated.

--- src/pentobi_kde_thumbnailer/PentobiThumbCreator.cpp
+++ src/pentobi_kde_thumbnailer/PentobiThumbCreator.cpp
@@ -25,6 +25,8 @@ bool PentobiThumbCreator::create(const QString& path, int width, int height,
                                  QImage& image)
 {
     image = QImage(width, height, QImage::Format_ARGB32);
+    if (image.isNull())
+        return false;
     image.fill(Qt::transparent);
     return createThumbnail(path, width, height, image);
 }
