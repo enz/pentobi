2747a626690b7d2677011416ae865603c30f75d5
Markus Enzenberger <enz@users.sourceforge.net>
2016-02-14 14:08:53 +0100

    Optimized symmetry detection.

--- src/libpentobi_mcts/StateUtil.cpp
+++ src/libpentobi_mcts/StateUtil.cpp
@@ -14,6 +14,7 @@ namespace libpentobi_mcts {
 
 using libpentobi_base::Color;
 using libpentobi_base::ColorMove;
+using libpentobi_base::Geometry;
 using libpentobi_base::Point;
 using libpentobi_base::PointState;
 
@@ -32,16 +33,27 @@ bool check_symmetry_broken(const Board& bd,
                            const SymmetricPoints& symmetric_points)
 {
     Color to_play = bd.get_to_play();
+    auto& geo = bd.get_geometry();
+    // No need to iterator over the whole board when checking symmetry (this
+    // makes the assumption that the symmetric points of the points in the
+    // first half of the integer range are in the second half).
+    Geometry::Iterator begin = geo.begin();
+    LIBBOARDGAME_ASSERT(geo.get_range() % 2 == 0);
+    Geometry::Iterator end(static_cast<Point::IntType>(geo.get_range() / 2));
+#if LIBBOARDGAME_DEBUG
+    for (auto p = begin; p != end; ++p)
+        LIBBOARDGAME_ASSERT(symmetric_points[*p].to_int() >= (*end).to_int());
+#endif
     if (to_play == Color(0) || to_play == Color(2))
     {
         // First player to play: the symmetry is broken if the position is
         // not symmetric.
-        for (Point p : bd)
+        for (auto p = begin; p != end; ++p)
         {
-            PointState s1 = bd.get_point_state(p);
+            PointState s1 = bd.get_point_state(*p);
             if (! s1.is_empty())
             {
-                Point symm_p = symmetric_points[p];
+                Point symm_p = symmetric_points[*p];
                 PointState s2 = bd.get_point_state(symm_p);
                 if (s2 != symmetric_state[s1.to_int()])
                     return true;
@@ -64,16 +76,18 @@ bool check_symmetry_broken(const Board& bd,
             // Don't try to handle non-alternating moves in board history
             return true;
         auto points = bd.get_move_points(last_mv.move);
-        for (Point p : bd)
+        for (Point p : points)
+            if (! bd.get_point_state(symmetric_points[p]).is_empty())
+                return true;
+        for (auto p = begin; p != end; ++p)
         {
-            PointState s1 = bd.get_point_state(p);
+            PointState s1 = bd.get_point_state(*p);
             if (! s1.is_empty())
             {
-                Point symm_p = symmetric_points[p];
-                PointState s2 = bd.get_point_state(symm_p);
-                if (s2 != symmetric_state[s1.to_int()])
-                    if (! (points.contains(p) && s2.is_empty()))
-                        return true;
+                PointState s2 = bd.get_point_state(symmetric_points[*p]);
+                if (s2 != symmetric_state[s1.to_int()]
+                        && ! points.contains(*p))
+                    return true;
             }
         }
     }
