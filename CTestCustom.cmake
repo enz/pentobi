8b76bafa788d672e6beed6d887c202e2a5af8cf4
Markus Enzenberger <enz@users.sourceforge.net>
2017-01-11 09:09:25 +0100

    Better comment

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,4 +1,5 @@
-# Some versions of KDECMakeSettings add a test that globally checks all
-# appstream files in the project, insists on reverse domain IDs and fails
-# if no install_manifest.txt is in the project's base directory.
+# Some versions of KDECMakeSettings (used in src/pentobi_kde_thumbnailer)
+# automatically add a test that globally checks all appstream files in
+# the project and fails if they don't use reverse domain IDs (and would
+# even fail if they did because of bugs in the test).
 set(CTEST_CUSTOM_TESTS_IGNORE appstreamtest)
