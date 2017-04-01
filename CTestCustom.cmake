767d683f43fa62f0f6145d7b2d5427a654a7a169
Markus Enzenberger <enz@users.sourceforge.net>
2017-04-01 18:09:42 +0200

    Removed workaround for disabling appstreamtest of KDECMakeSettings after the appstream files have been updated to not make it fail.

--- CTestCustom.cmake
+++
@@ -1,5 +0,0 @@
-# Some versions of KDECMakeSettings (used in src/pentobi_kde_thumbnailer)
-# automatically add a test that globally checks all appstream files in
-# the project and fails if the build directory does not contain
-# install_manifest.txt (which exists only after installation).
-set(CTEST_CUSTOM_TESTS_IGNORE appstreamtest)
