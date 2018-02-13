7be5f917351e8ec50f72d063fd3fb3863cbf4396
Markus Enzenberger <markus.enzenberger@gmail.com>
2018-02-13 20:07:39 +0000

    Added translation using Weblate (French)

---
+++ src/pentobi_qml/qml/i18n/replace_qtbase_fr.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <source>Cancel</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
