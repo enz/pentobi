624bf9cbe37cd93df408d74d8051d6074d7f03d3
Markus Enzenberger <markus.enzenberger@gmail.com>
2018-02-15 13:43:11 +0000

    Added translation using Weblate (French)

---
+++ src/libpentobi_gui/translations/libpentobi_gui_fr.ts
@@ -0,0 +1,186 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>ComputerColorDialog</name>
+    <message>
+        <source>Computer Colors</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Computer plays:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Blue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Green</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Yellow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Red</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Blue/Red</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Yellow/Green</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Purple</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Orange</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>GameInfoDialog</name>
+    <message>
+        <source>Game Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Blue:</source>
+        <oldsource>Player Blue:</oldsource>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Green:</source>
+        <oldsource>Player Green:</oldsource>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Yellow:</source>
+        <oldsource>Player Yellow:</oldsource>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Red:</source>
+        <oldsource>Player Red:</oldsource>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Blue/Red:</source>
+        <oldsource>Player Blue/Red:</oldsource>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Yellow/Green:</source>
+        <oldsource>Player Yellow/Green:</oldsource>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Date:</source>
+        <oldsource>Date:</oldsource>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Time limits:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Event:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>R&amp;ound:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Purple:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player &amp;Orange:</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>HelpWindow</name>
+    <message>
+        <source>Back</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Show previous page in history</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Forward</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Show next page in history</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>InitialRatingDialog</name>
+    <message>
+        <source>Initial Rating</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>You have not yet played rated games in this game variant. Estimate your playing strength to initialize your rating.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Beginner</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Expert</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Your initial rating: %1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Util</name>
+    <message>
+        <source>Blue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Yellow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Blue/Red</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Yellow/Green</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Purple</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Orange</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
