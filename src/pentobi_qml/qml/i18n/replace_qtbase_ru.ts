c2e3287581ba3337e66ef154a33b71eab726cbd5
Markus Enzenberger <markus.enzenberger@gmail.com>
2018-02-17 14:34:40 +0000

    Added translation using Weblate (Russian)

---
+++ src/pentobi_qml/qml/i18n/replace_qtbase_ru.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <source>Cancel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
