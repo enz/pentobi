fcb595516d820b65873c7bf416484c3196fd16ea
Markus Enzenberger <enz@users.sourceforge.net>
2018-08-12 11:53:39 +0200

    Revert "The thumbnailer now produces crisper images because it will use a whole" Extra transparent border doesn't look good in Dolphin. This reverts commit d93a1fbf355156502da6506d134e54f8d19261d2.

--- src/libpentobi_thumbnail/CreateThumbnail.cpp
+++ src/libpentobi_thumbnail/CreateThumbnail.cpp
@@ -135,7 +135,8 @@ bool getFinalPosition(const SgfNode& root, Variant& variant,
 
 //-----------------------------------------------------------------------------
 
-bool createThumbnail(const QString& path, int width, int height, QImage& image)
+bool createThumbnail(const QString& path, int width, int height,
+                     QImage& image)
 {
     TreeReader reader;
     reader.set_read_only_main_variation(true);
@@ -151,42 +152,9 @@ bool createThumbnail(const QString& path, int width, int height, QImage& image)
         cerr << "Not a valid Blokus SGF file\n";
         return false;
     }
-    // Prefer pixel alignment if it doesn't reduce the size by more than 10%
-    auto geoWidth = static_cast<int>(geo->get_width());
-    auto geoHeight = static_cast<int>(geo->get_height());
-    int preferredWidth;
-    int preferredHeight;
-    auto pieceSet = get_piece_set(variant);
-    if (pieceSet == PieceSet::nexos)
-    {
-        preferredWidth = static_cast<int>(floor(width / (geoWidth - 0.5))
-                                          * (geoWidth - 0.5));
-        preferredHeight = preferredWidth;
-    }
-    else if (pieceSet == PieceSet::gembloq)
-    {
-        preferredWidth = (2 * width) / (2 * geoWidth) * geoWidth;
-        preferredHeight = preferredWidth;
-    }
-    else
-    {
-        preferredWidth = width / geoWidth * geoWidth;
-        preferredHeight = height / geoHeight * geoHeight;
-    }
-    if (preferredWidth < 0.9 * width)
-    {
-        preferredWidth = width;
-        preferredHeight = height;
-    }
     QPainter painter(&image);
-    auto offX = (width - preferredWidth) / 2;
-    auto offY = (height - preferredHeight) / 2;
-    painter.translate(offX, offY);
     BoardPainter boardPainter;
-    boardPainter.paintEmptyBoard(painter,
-                                 static_cast<unsigned>(preferredWidth),
-                                 static_cast<unsigned>(preferredHeight),
-                                 variant, *geo);
+    boardPainter.paintEmptyBoard(painter, width, height, variant, *geo);
     boardPainter.paintPieces(painter, pointState, pieceId);
     return true;
 }
