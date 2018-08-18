67c8f84970e742da0217102b2a51fcc816a9a82b
Markus Enzenberger <enz@users.sourceforge.net>
2018-08-18 16:51:40 +0200

    Removed unused include

--- src/pentobi/PieceModel.cpp
+++ src/pentobi/PieceModel.cpp
@@ -11,7 +11,6 @@
 #include "PieceModel.h"
 
 #include "libboardgame_base/RectTransform.h"
-#include "libboardgame_sys/Compiler.h"
 #include "libpentobi_base/Board.h"
 #include "libpentobi_base/GembloQTransform.h"
 #include "libpentobi_base/TrigonTransform.h"
