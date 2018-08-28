838b61c7b7479da3f7076066a8b0a356e2864a02
Markus Enzenberger <enz@users.sourceforge.net>
2018-08-28 13:21:20 +0200

    Added unit test for BoardConst::from_string("null")

--- src/unittest/libpentobi_base/BoardConstTest.cpp
+++ src/unittest/libpentobi_base/BoardConstTest.cpp
@@ -13,11 +13,21 @@ using namespace libpentobi_base;
 
 //-----------------------------------------------------------------------------
 
+/** Test that from_string() handles null moves.
+    Used for example in pentobi/AnalyzeGameMode.cpp */
+LIBBOARDGAME_TEST_CASE(pentobi_base_board_const_from_string_null)
+{
+    auto& bc = BoardConst::get(Variant::duo);
+    Move mv;
+    LIBBOARDGAME_CHECK(bc.from_string(mv, "null"));
+    LIBBOARDGAME_CHECK(mv.is_null());
+}
+
 /** Test that points in move strings are ordered.
     As specified in doc/blksgf/Pentobi-SGF.html, the order should be
     (a1, b1, ..., a2, b2, ...). There is no restriction on the order when
     parsing move strings in from_string(). */
-LIBBOARDGAME_TEST_CASE(pentobi_base_board_const_move_string)
+LIBBOARDGAME_TEST_CASE(pentobi_base_board_const_to_string_ordered)
 {
     auto& bc = BoardConst::get(Variant::duo);
     Move mv;
