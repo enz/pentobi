5ecdb36954b8157406706e653b15dfb5b7fd8bd0
Markus Enzenberger <enz@users.sourceforge.net>
2019-02-02 10:04:50 +0100

    Added comment

--- pentobi_kde_thumbnailer/CTestCustom.cmake
+++ pentobi_kde_thumbnailer/CTestCustom.cmake
@@ -1,4 +1,4 @@
-# We don't want to run appstreamtest added by KDECMakeSettings if
-# PENTOBI_BUILD_KDE_THUMBNAILER because it requires an Internet connection
-# to check the screenshot images.
+# We don't want to run appstreamtest added by KDECMakeSettings because it
+# requires an Internet connection to check the screenshot images. This file
+# must be copied to the build root directory to disable the test.
 set(CTEST_CUSTOM_TESTS_IGNORE appstreamtest)
