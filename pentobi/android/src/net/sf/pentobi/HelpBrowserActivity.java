ebb8d5f35d07a43af85c373891a6ee182ac7392c
Markus Enzenberger <enz@users.sourceforge.net>
2020-12-07 21:33:03 +0100

    Avoid FileUriExposedException with WebView on newer Android versions.

--- pentobi/android/src/net/sf/pentobi/HelpBrowserActivity.java
+++ pentobi/android/src/net/sf/pentobi/HelpBrowserActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.net.Uri;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class HelpBrowserActivity
     extends Activity
@@ -16,6 +17,15 @@ public class HelpBrowserActivity
             super.onCreate(savedInstanceState);
             setContentView(R.layout.help_browser_activity);
             WebView webView = (WebView)findViewById(R.id.webview);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view,
+                                                        String url)
+                {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
             webView.loadUrl(getIntent().getData().toString());
         }
         catch (Exception e)
