d33b1c51a585003ac09adf2c270cf98acae07a60
Markus Enzenberger <enz@users.sourceforge.net>
2026-04-28 16:19:03 +0200

    Removed redundant check for mv.is_null()

--- libpentobi_mcts/Util.cpp
+++ libpentobi_mcts/Util.cpp
@@ -48,8 +48,7 @@ void dump_tree_recurse(Writer& writer, Variant variant,
         {
             auto& board_const = BoardConst::get(variant);
             auto id = get_color_id(variant, to_play);
-            if (! mv.is_null())
-                writer.write_property(id, board_const.to_string(mv, false));
+            writer.write_property(id, board_const.to_string(mv, false));
         }
         dump_tree_recurse(writer, variant, tree, *i, next_to_play);
         writer.end_tree();
