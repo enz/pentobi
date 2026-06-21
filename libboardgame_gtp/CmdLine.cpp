b58c3052f2afb28e579ca47137d1a1f267bd7a92
Markus Enzenberger <enz@users.sourceforge.net>
2026-06-21 15:08:25 +0200

    Removed broken backslash handling in CmdLine

--- libboardgame_gtp/CmdLine.cpp
+++ libboardgame_gtp/CmdLine.cpp
@@ -32,18 +32,17 @@ void CmdLine::add_elem(string::const_iterator begin,
 /** Find elements (ID, command name, arguments).
     Arguments are words separated by whitespaces.
     Arguments with whitespaces can be quoted with quotation marks ('"').
-    Characters can be escaped with a backslash ('\'). */
+    Escaping is not supported. */
 void CmdLine::find_elem()
 {
     m_elem.clear();
-    bool escape = false;
     bool is_in_string = false;
     string::const_iterator begin = m_line.begin();
     string::const_iterator i;
     for (i = begin; i < m_line.end(); ++i)
     {
         char c = *i;
-        if (c == '"' && ! escape)
+        if (c == '"')
         {
             if (is_in_string)
                 add_elem(begin, i);
@@ -56,7 +55,6 @@ void CmdLine::find_elem()
                 m_elem.emplace_back(&*begin, i - begin);
             begin = i + 1;
         }
-        escape = (c == '\\' && ! escape);
     }
     if (i > begin)
         m_elem.emplace_back(&*begin, m_line.end() - begin);
