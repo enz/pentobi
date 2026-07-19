78bd848206d4d47f4fa4a7ee680baa371c911375
Markus Enzenberger <enz@users.sourceforge.net>
2026-07-19 13:33:45 +0200

    Revert "New config option PENTOBI_USE_QAPPLICATION to make native file"
    This reverts commit 4738223dcae130f1ce841204e0fcc94a7f378e62.

--- pentobi/Main.cpp
+++ pentobi/Main.cpp
@@ -5,6 +5,7 @@
 //-----------------------------------------------------------------------------
 
 #include <QCommandLineParser>
+#include <QGuiApplication>
 #include <QIcon>
 #include <QLibraryInfo>
 #include <QQmlApplicationEngine>
@@ -16,12 +17,6 @@
 #include "ImageProvider.h"
 #include "PlayerModel.h"
 #include "libboardgame_base/Log.h"
-
-#ifdef PENTOBI_USE_QAPPLICATION
-#include <QApplication>
-#else
-#include <QGuiApplication>
-#endif
 
 using namespace Qt::StringLiterals;
 
@@ -60,12 +55,7 @@ int main(int argc, char *argv[])
 #ifdef VERSION
     QCoreApplication::setApplicationVersion(QStringLiteral(VERSION));
 #endif
-#ifdef PENTOBI_USE_QAPPLICATION
-    // QApplication still needed for native file dialogs in KDE platform theme
-    QApplication app(argc, argv);
-#else
     QGuiApplication app(argc, argv);
-#endif
     QLocale locale;
     QTranslator qtTranslator;
     if (qtTranslator.load(locale, "qt"_L1, "_"_L1,
